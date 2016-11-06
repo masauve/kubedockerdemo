2606564ad64f542e295d9ba4564c726b251f485e
Martin Sauve <martin.sauve@gmail.com>
2016-11-05 21:43:22 -0400

    changed portw

--- watch_update.sh
+++ watch_update.sh
@@ -3,7 +3,7 @@ IP=$(kubectl --namespace=kubedemo get services mynode -o yaml \
   | cut -f2 -d:)
 
 for i in $(seq 1 1000); do
-  curl --connect-timeout 1 -s $IP:8080;
+  curl --connect-timeout 1 -s $IP:8000;
   echo
   sleep 1;
 done
